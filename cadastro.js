9a8c36304a306e5c24af76b3a2e8a0e7eddb714f
Amarildo <69056178+AmarildoOliveira84@users.noreply.github.com>
2020-10-25 22:03:16 -0400

    Cadastro
    Cadastro

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function abrir() {
+    document.getElementById("menu").classList.add("abre")
+}
+
+function fechar() {
+    document.getElementById("menu").classList.remove("abre")
+}
+
+function fecharMensagem(){
+    document.getElementById("mensagens").classList.add("mensagem")
+}
---
+++ cadastroValidacao.js
@@ -0,0 +1,53 @@
+class Cadastro {
+    validar() {
+        let mensagem = ""
+        let nome = document.getElementById("nome").value
+        let email = document.getElementById("email").value
+        let sexo = document.querySelector("[name=sexo]:checked")
+        let estadoCivil = document.querySelector("[name=civil]:checked")
+        let curso = document.querySelector("[name=curso]:checked")
+        let estado = document.getElementById("estado").value
+        let foto = document.getElementById("foto").files[0]
+        let nascimento = document.getElementById("data").value
+
+        if (nome == "") {
+            mensagem = "Preencha o campo nome\n"
+        }
+        if (email == "") {
+            mensagem = mensagem + "Preencha o campo e-mail\n"
+        }
+        if (sexo == null) {
+            mensagem = mensagem + "Preencha o campo sexo\n"
+        }
+        if (estadoCivil == null) {
+            mensagem = mensagem + "Preencha o campo Estado Civil\n"
+        }
+        if (curso == null) {
+            mensagem = mensagem + "Preencha o campo curso\n"
+        }
+        if (estado == "") {
+            mensagem = mensagem + "Preencha o campo estado\n"
+        }
+        if (foto == undefined) {
+            mensagem = mensagem + "Preencha o campo foto\n"
+        }
+        else if (foto.size > 1048576) {
+            mensagem = mensagem + "Preencha a foto deve ter 1MB\n"
+        } else if (foto.type != "image/png" && foto.type != "image/jpeg") {
+            mensagem = mensagem + "Preencha a foto em Formato inválido\n"
+        }
+        if (nascimento == "") {
+            mensagem = mensagem + "Informe a data do nascimento\n"
+        }
+
+        if (mensagem != "") {
+            
+            document.getElementById("mensagens").classList.remove("mensagem")
+            document.getElementById("texto").innerText = mensagem
+            
+        }
+
+
+    }
+}
+var c = new Cadastro()
